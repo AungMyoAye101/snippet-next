a9302cad3863ddf13f5250aefdcc10550e0e0abf
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-07 19:25:52 +0630

    add edit function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,24 @@
 "use server";
 
 import { db } from "@/db";
+import { redirect } from "next/navigation";
 
 export const editSnippet = async (id: number, code: string) => {
-  console.log(id, code);
+  await db.snippet.update({
+    where: {
+      id,
+    },
+    data: {
+      code,
+    },
+  });
+  redirect(`/snippets/${id}`);
+};
+
+// delete snippet
+export const deleteSnippet = async (id: number) => {
+  await db.snippet.delete({
+    where: { id },
+  });
+  redirect("/");
 };
