71ae9a70d8894b99d18c7669074d9d29024f9e76
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-07 19:33:39 +0630

    add delete function

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
-
+import * as actions from "@/actions";
 const SnippetShowPage = async ({ params }: { params: { id: string } }) => {
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(params.id) },
@@ -10,6 +10,8 @@ const SnippetShowPage = async ({ params }: { params: { id: string } }) => {
   if (!snippet) {
     notFound();
   }
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
   return (
     <div className="flex flex-col gap-2 px-6 py-10 rounded-md shadow border ">
       <h1 className="text-lg font-bold ">{snippet.title} </h1>
@@ -20,9 +22,11 @@ const SnippetShowPage = async ({ params }: { params: { id: string } }) => {
         >
           Edit
         </Link>
-        <button className="px-4 py-2 rounded shadow text-sm hover:scale-95 transition-transform ease-out border hover:border-green-300">
-          Delete
-        </button>
+        <form action={deleteSnippetAction}>
+          <button className="px-4 py-2 rounded shadow text-sm hover:scale-95 transition-transform ease-out border hover:border-green-300">
+            Delete
+          </button>
+        </form>
       </div>
       <pre className="p-3 border border-gray-200 bg-zinc-100 rounded">
         <code>{snippet.code}</code>
