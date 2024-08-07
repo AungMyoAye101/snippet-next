d8eeed6b0accb435b8adc3493223a941a9324cd2
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-07 20:06:41 +0630

    add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,17 @@ import Link from "next/link";
 export default async function Home() {
   const snippets = await db.snippet.findMany();
   const renderSnippets = snippets.map((snippet) => (
-    <div key={snippet.id}>
-      <Link href={`/snippets/${snippet.id}`}>{snippet.title}</Link>
+    <div
+      key={snippet.id}
+      className="flex justify-between items-center rounded border border-gray-200 px-4 py-2 hover:bg-green-300"
+    >
+      <h1 className="text-md font-semibold font-serif">{snippet.title}</h1>
+      <Link
+        href={`/snippets/${snippet.id}`}
+        className="py-2 px-4 border border-gray-300 rounded text-sm text-white bg-blue-500 hover:bg-blue-600"
+      >
+        view
+      </Link>
     </div>
   ));
   return (
@@ -20,9 +29,7 @@ export default async function Home() {
         </Link>
       </div>
 
-      <div className="flex flex-col gap-2  px-4 py-6 rounded-md shadow border ">
-        {renderSnippets}
-      </div>
+      <div className="flex flex-col gap-2  ">{renderSnippets}</div>
     </div>
   );
 }
